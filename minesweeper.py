a56e531737eee412bb8efff82a5c900922e0631f
KingIradescense <133372035+KingIradescense@users.noreply.github.com>
2025-05-19 02:20:12 -0400

    print testing. hate it here

--- minesweeper.py
+++ minesweeper.py
@@ -1,12 +1,13 @@
 import random
+from collections import deque
 
 # determine difficulty of the game
 
-difficult = {
-    "easy" = (8, 8, 10),
-    "medium" = (16, 16, 40),
-    "hard" = (15, 15, 100),
-    "nightmare" = (20, 20, 200)
+difficulty = {
+    "easy" : (8, 8, 10),
+    "medium" : (16, 16, 40),
+    "hard" : (15, 15, 100),
+    "nightmare" : (20, 20, 200)
 }
 
 # generate a board with bombs and hints, takes difficulty as arg
@@ -14,14 +15,14 @@ difficult = {
 def generate_board(level):
     w, h, bombs = difficulty[level]
 
-    board = [[0 in _ for range(w)] for _ in range(h)]
+    board = [[0 for _ in range(w)] for _ in range(h)]
     coords = [(i, j) for i in range(w) for j in range(h)]
 
     # place bombs
 
     random.shuffle(coords)
 
-    for bomb in bombs:
+    for _ in range(bombs):
         a, b = coords.pop()
         board[a][b] = -1
 
@@ -30,8 +31,30 @@ def generate_board(level):
         for dx in [-1, 0, 1]:
             for dy in [-1, 0, 1]:
                 nx, ny = dx + a, dy + b
-                if 0 <= nx < len(board) and 0 <= ny len(board[0]) and board[nx][ny] != -1:
+                if 0 <= nx < len(board) and 0 <= ny < len(board[0]) and board[nx][ny] != -1:
                     board[nx][ny] += 1
 
     return board
         
+# flood fill the board when a blank space is revealed
+
+def flood_fill(i, j, puzzle, board):
+    queue = dequeue()
+    enqueued = set()
+
+    queue.append((i, j))
+    enqueued.add((i, j))
+
+# print board for testing
+
+def print_board(board):
+    for row in board:
+        for cell in row:
+            print (cell, end = " ")
+        print()
+    print()
+
+if __name__ == "__main__":
+    level = "easy"
+    board = generate_board(level)
+    print_board(board)
